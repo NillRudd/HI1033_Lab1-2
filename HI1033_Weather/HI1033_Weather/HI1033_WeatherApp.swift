2e1e081008ef3ffdf6146e5f9fdae419a72f4309
Niklas Roslund <nroslund@kth.se>
2023-11-27 13:06:40 +0100

    Project init

---
+++ HI1033_Weather/HI1033_Weather/HI1033_WeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  HI1033_WeatherApp.swift
+//  HI1033_Weather
+//
+//  Created by Niklas Roslund on 2023-11-27.
+//
+
+import SwiftUI
+
+@main
+struct HI1033_WeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
