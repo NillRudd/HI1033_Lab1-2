27d4f2485cb1252cd2203ae64b10431e3d9a68b8
estebanmasaya <estebanmasaya@gmail.com>
2023-12-03 21:23:31 +0100

    Fetching weather must happen only if fetchiing coordinates is done. Completion handler to fix it!

--- HI1033_Weather/HI1033_Weather/ViewModel/WeatherVM.swift
+++ HI1033_Weather/HI1033_Weather/ViewModel/WeatherVM.swift
@@ -20,7 +20,7 @@ class WeatherVM : ObservableObject {
     var places : [String] {
         theModel.places
     }
-
+    
     var lastUpdated: Date{
         theModel.lastUpdated
     }
@@ -30,7 +30,7 @@ class WeatherVM : ObservableObject {
     var latitude: Double {
         theModel.latitude
     }
-
+    
     var longitude: Double {
         theModel.longitude
     }
@@ -41,30 +41,33 @@ class WeatherVM : ObservableObject {
         weatherData = WeatherData()
         testNetwork()
     }
-
+    
     func getIconWithWeatherCode(code: Int) -> String {
         return theModel.iconFromCode(code: code)
     }
     
-    func testNetwork(){
+    func testNetwork() {
         let monitor = NWPathMonitor()
         monitor.pathUpdateHandler = { path in
             DispatchQueue.main.async {
                 if path.status == .satisfied {
                     print("We're connected!")
-
-                    self.fetchGeoData()
-                    self.getDataFromWeb()
-                    self.theModel.setLastUpdated()
-                    self.isConnected = true
-                        //self.theModel.updateWeatherData()
-                    //}
-
+                    
+                    self.fetchGeoData { success in
+                        if success {
+                            self.getDataFromWeb()
+                            self.theModel.setLastUpdated()
+                            self.isConnected = true
+                        } else {
+                            self.getDataFromPersistence()
+                            self.isConnected = false
+                        }
+                    }
+                    
                 } else {
                     self.getDataFromPersistence()
                     self.isConnected = false
                     print("No connection.")
-                    self.getDataFromPersistence()
                 }
                 print(path.isExpensive)
             }
@@ -73,130 +76,134 @@ class WeatherVM : ObservableObject {
         monitor.start(queue: queue)
     }
     
-    func fetchGeoData() {
-        guard let encodedLocationString = locationInput.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-            print("Failed to encode location string")
-            return
-        }
-        guard let endpoint = URL(string: "https://www.smhi.se/wpt-a/backend_solr/autocomplete/search/\(encodedLocationString)") else {
-            print("Invalid URL")
-            return
-        }
-        let task = URLSession.shared.dataTask(with: endpoint) { data, response, error in
-            if let error = error {
-                print("Network request error: \(error)")
+    func fetchGeoData(completion: @escaping (Bool) -> Void) {
+            guard let encodedLocationString = locationInput.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+                print("Failed to encode location string")
+                completion(false)
                 return
             }
-            guard let data = data else {
-                print("No data received")
+            guard let endpoint = URL(string: "https://www.smhi.se/wpt-a/backend_solr/autocomplete/search/\(encodedLocationString)") else {
+                print("Invalid URL")
+                completion(false)
                 return
             }
-            do {
-                print("Decoding JSON...")
-                guard let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
-                    print("Failed to parse JSON as array of dictionaries")
+            let task = URLSession.shared.dataTask(with: endpoint) { data, response, error in
+                if let error = error {
+                    print("Network request error: \(error)")
+                    completion(false)
                     return
                 }
-                //print(jsonArray )
-                if let firstJson = jsonArray.first {
-                    do {
-                        let geoData = try JSONDecoder().decode(GeoData.self, from: JSONSerialization.data(withJSONObject: firstJson))
-                        DispatchQueue.main.async {
-    
-                            self.theModel.setCoordinates(latitude: geoData.lat, longitude: geoData.lon)
-                            self.theModel.setLocation(location: geoData.place)
-                            //self.theModel.getData() {
-                                //self.theModel.updateWeatherData()
-                            //}
-
-                            self.objectWillChange.send()
+                guard let data = data else {
+                    print("No data received")
+                    completion(false)
+                    return
+                }
+                do {
+                    print("Decoding JSON...")
+                    guard let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
+                        print("Failed to parse JSON as an array of dictionaries")
+                        completion(false)
+                        return
+                    }
+                    if let firstJson = jsonArray.first {
+                        do {
+                            let geoData = try JSONDecoder().decode(GeoData.self, from: JSONSerialization.data(withJSONObject: firstJson))
+                            DispatchQueue.main.async {
+                                self.theModel.setCoordinates(latitude: geoData.lat, longitude: geoData.lon)
+                                self.theModel.setLocation(location: geoData.place)
+                                self.objectWillChange.send()
+                                print("geodata Updated")
+                                completion(true)
+                            }
+                            
+                        } catch {
+                            print("Failed to decode GeoData for JSON: \(firstJson), Error: \(error)")
+                            completion(false)
                         }
-                        
-                    } catch {
-                        print("Failed to decode GeoData for JSON: \(firstJson), Error: \(error)")
                     }
-                }
-                
-            } catch {
-                print("Failed to parse JSON: \(error)")
-            }
-        }
-        task.resume()
-    }
-    
-    func getDataFromWeb() {
-        let endpoint = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=\(latitude)&longitude=\(longitude)&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=GMT")!
-        let sessionConfig = URLSessionConfiguration.default
-        let session = URLSession(configuration: sessionConfig)
-        
-        let task = session.dataTask(with: endpoint) { (data, response, error) in
-            if let error = error {
-                print("error: \(error.localizedDescription)")
-            } else if let data = data {
-                if var jsonData = try? JSONDecoder().decode(WeatherData.self, from: data) {
-                    self.persistenceController.saveWeatherData(weatherData: jsonData)
-                    DispatchQueue.main.async {
-                        jsonData.timestamp = Date.now
-                        self.weatherData = jsonData
-                    }
-                } else {
-                    print("Failed to decode JSON into WeatherData")
+                } catch {
+                    print("Failed to parse JSON: \(error)")
+                    completion(false)
                 }
             }
+            task.resume()
         }
-        task.resume()
         
-        session.finishTasksAndInvalidate()
-        print("finished get data")
-    }
-    
-    func getDataFromPersistence() {
-        self.weatherData = persistenceController.fetchWeatherData()?.last ?? WeatherData()
-    }
-
-    func formatDatetoHour(timestamp: String) -> String{
-        let dateFormatter = DateFormatter()
-        var hour = ""
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm"
-        if let date = dateFormatter.date(from: timestamp) {
-            let calendar = Calendar.current
-            hour = String(calendar.component(.hour, from: date))
-        } else {
-            //print("Failed to parse the date string: \(timestamp)")
+        func getDataFromWeb() {
+            print("start getting weather")
+            let endpoint = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=\(latitude)&longitude=\(longitude)&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=GMT")!
+            let sessionConfig = URLSessionConfiguration.default
+            let session = URLSession(configuration: sessionConfig)
+            
+            let task = session.dataTask(with: endpoint) { (data, response, error) in
+                if let error = error {
+                    print("error: \(error.localizedDescription)")
+                } else if let data = data {
+                    if var jsonData = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                        self.persistenceController.saveWeatherData(weatherData: jsonData)
+                        DispatchQueue.main.async {
+                            jsonData.timestamp = Date.now
+                            self.weatherData = jsonData
+                            print("updating weather")
+                        }
+                    } else {
+                        print("Failed to decode JSON into WeatherData")
+                    }
+                }
+            }
+            task.resume()
+            
+            session.finishTasksAndInvalidate()
+            print("finished get data")
         }
-        return hour
-    }
-    
-    func formatDatetoWeekDay(timestamp: String) -> String{
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        if let date = dateFormatter.date(from: timestamp){
-            dateFormatter.dateFormat = "EEEE"
-            return dateFormatter.string(from: date)
-        } else {
-            //print("Failed to parse the date string to a WeekDay: \(timestamp)")
+        
+        func getDataFromPersistence() {
+            self.weatherData = persistenceController.fetchWeatherData()?.last ?? WeatherData()
         }
-        return ""
+        
+        func formatDatetoHour(timestamp: String) -> String{
+            let dateFormatter = DateFormatter()
+            var hour = ""
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm"
+            if let date = dateFormatter.date(from: timestamp) {
+                let calendar = Calendar.current
+                hour = String(calendar.component(.hour, from: date))
+            } else {
+                //print("Failed to parse the date string: \(timestamp)")
+            }
+            return hour
+        }
+        
+        func formatDatetoWeekDay(timestamp: String) -> String{
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            if let date = dateFormatter.date(from: timestamp){
+                dateFormatter.dateFormat = "EEEE"
+                return dateFormatter.string(from: date)
+            } else {
+                //print("Failed to parse the date string to a WeekDay: \(timestamp)")
+            }
+            return ""
+        }
+        
+        func addToFavorites(place: String) {
+            theModel.addPlace(place: place)
+        }
+        
+        func removeFromFavorites(place: String) {
+            theModel.removePlace(place: place)
+        }
+        
+        func formatDateLastUpdated(timestamp: Date) -> String{
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            return dateFormatter.string(from: timestamp)
+        }
     }
     
-    func addToFavorites(place: String) {
-        theModel.addPlace(place: place)
+    extension String {
+        func localized() -> String {
+            NSLocalizedString(self, comment: "")
+        }
     }
     
-    func removeFromFavorites(place: String) {
-        theModel.removePlace(place: place)
-    }
-    
-    func formatDateLastUpdated(timestamp: Date) -> String{
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        return dateFormatter.string(from: timestamp)
-    }
-}
-
-extension String {
-    func localized() -> String {
-        NSLocalizedString(self, comment: "")
-    }
-}
-
